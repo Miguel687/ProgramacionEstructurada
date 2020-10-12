10deb2bec4840c7e76b45866c88a1d8435c278e2
Miguel687 <61020779+Miguel687@users.noreply.github.com>
2020-10-11 23:18:49 -0500

    Update Main.java

--- shuntingyard/Main.java
+++ shuntingyard/Main.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 /**
- * Created by lwm on 03.09.16.
+ * Created by Miky
  */
 public class Main {
     public static void main(String[] args) throws IOException {
